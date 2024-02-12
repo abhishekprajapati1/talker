35d17b1b6c0367c086dd45a10f5ce9c16c259304
abhishekprajapati1 <ap0661236@gmail.com>
2024-02-12 10:08:13 +0530

    :fire: added modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { ContactModule } from './contact/contact.module';
 
 @Module({
-  imports: [],
   controllers: [AppController],
   providers: [AppService],
+  imports: [UserModule, AuthModule, ContactModule],
 })
 export class AppModule {}
